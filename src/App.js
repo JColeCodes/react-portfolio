03510f8fe63961e850f31c987c63da0f0267a035
Jennifer Cole <capauldi@gmail.com>
2022-03-19 03:51:15 -0700

    project info created and added thumbnails

--- src/App.js
+++ src/App.js
@@ -1,8 +1,10 @@
 import React from 'react';
 import Header from './components/Header';
 import Footer from './components/Footer';
+import { projectInfo } from './assets/project-info';
 
 function App() {
+  const projects = projectInfo;
   return (
     <div className="container">
       {/* Header */}
---
+++ src/assets/project-info.js
@@ -0,0 +1,44 @@
+export const projectInfo = [
+    {
+        title: 'RANDM',
+        technologies: ["Node.js", "MySQL", "Express", "Socket.io", "Handlebars", "Sequelize", "REST API", "CSS"],
+        thumbnail: 'Randm.jpg',
+        repo: 'https://github.com/JColeCodes/randm',
+        url: 'https://ran-dm.herokuapp.com/'
+    },
+    {
+        title: 'Tech Blog',
+        technologies: ["Node.js", "MySQL", "Express", "Handlebars", "Sequelize", "REST API", "CSS"],
+        thumbnail: 'TechBlog.png',
+        repo: 'https://github.com/JColeCodes/tracking-tech',
+        url: 'https://tech-blog-jcolecodes.herokuapp.com/'
+    },
+    {
+        title: 'Travel Itinerary',
+        technologies: ["HTML", "JavaScript", "jQuery", "DOM", "API", "LocalStorage", "Moment.js", "Foundation / CSS"],
+        thumbnail: 'TravelItinerary.jpg',
+        repo: 'https://github.com/JColeCodes/travel-itinerary',
+        url: 'https://jcolecodes.github.io/travel-itinerary/'
+    },
+    {
+        title: 'Weather Dashboard',
+        technologies: ["HTML", "CSS", "JavaScript", "DOM", "API"],
+        thumbnail: 'WeatherDashboard.jpg',
+        repo: 'https://github.com/JColeCodes/climate-checker',
+        url: 'https://jcolecodes.github.io/climate-checker/'
+    },
+    {
+        title: 'Code Quiz',
+        technologies: ["Javascript", "DOM", "CSS"],
+        thumbnail: 'CodeQuiz.gif',
+        repo: 'https://github.com/JColeCodes/program-exam',
+        url: 'https://jcolecodes.github.io/program-exam/'
+    },
+    {
+        title: 'Team Profile Generator',
+        technologies: ["Node.js", "Inquirer", "Jest", "HTML", "CSS"],
+        thumbnail: 'TeamProfileGenerator.jpg',
+        repo: 'https://github.com/JColeCodes/squad-dynamo',
+        url: 'https://jcolecodes.github.io/squad-dynamo/dist/'
+    }
+];
