b235ddb1b8cd9ffe996603f5db96a3249af2120a
Jennifer Cole <capauldi@gmail.com>
2022-03-26 04:32:34 -0700

    added form functionality

--- src/pages/Contact/index.js
+++ src/pages/Contact/index.js
@@ -1,4 +1,5 @@
 import React, { useState } from 'react';
+import axios from 'axios';
 import { validateEmail } from '../../utils/helpers';
 
 function Contact({ title }) {
@@ -8,10 +9,12 @@ function Contact({ title }) {
         { url: "https://www.instagram.com/capauldi/", icon: "fa-instagram", title: "Instagram (Personal)" },
         { url: "https://discord.com/users/354098549761507332", icon: "fa-discord", title: "Discord (Personal)" }
     ];
-    const [formState, setFormState] = useState({ name: '', email: '', subject: '', message: '' });
+
+    const [formStatus, setFormStatus] = useState(false);
+    const [formState, setFormState] = useState({ name: '', email: '', message: '' });
     const [errorMessage, setErrorMessage] = useState('');
 
-    const { name, email, subject, message } = formState;
+    const { name, email, message } = formState;
     function handleChange(e) {
         if (e.target.name === 'email') {
             const isValid = validateEmail(e.target.value);
@@ -41,6 +44,28 @@ function Contact({ title }) {
         }
     }
 
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        const formData = new FormData();
+        Object.entries(formState).forEach(([key, value]) => {
+            formData.append(key, value);
+        });
+
+        axios({
+            method: "post",
+            url: "https://getform.io/f/cc64cda4-aecd-4082-acac-7e87cbf56c1d",
+            data: formData,
+            headers: { "Content-Type": "multipart/form-data" }
+        })
+            .then(function(response) {
+                setFormStatus(true);
+                setFormState({ name: '', email: '', message: '' });
+            })
+            .catch(function(error) {
+                console.log(error);
+            });
+    }
+
     return(
         <section id="contact">
             <h3>{title}</h3>
@@ -50,15 +75,12 @@ function Contact({ title }) {
                 ))}
             </ul>
             <p>Above are some of my social media contacts, however, the best way to contact me is via email! You can send me a message through the form below. If things aren't working, you can send me an email directly at <a href="mailto:contact@colecodes.com">contact@colecodes.com</a>.</p>
-            <form id="contact-form" className="contact-form">
+            <form id="contact-form" className="contact-form" encType="multipart/form-data" onSubmit={handleSubmit}>
                 <label htmlFor="name">Name: <span className="required">*</span></label>
                 <input type="text" name="name" defaultValue={name} onBlur={handleChange} />
 
                 <label htmlFor="email">Email: <span className="required">*</span></label>
                 <input type="email" name="email" defaultValue={email} onBlur={handleChange} />
-
-                <label htmlFor="subject">Subject: <span className="required">*</span></label>
-                <input type="text" name="subject" defaultValue={subject} onBlur={handleChange} />
 
                 <label htmlFor="message">Message: <span className="required">*</span></label>
                 <textarea name="message" defaultValue={message} onBlur={handleChange} onChange={dynamicTextbox}></textarea>
@@ -66,6 +88,8 @@ function Contact({ title }) {
                 {errorMessage && <p className="required error">{errorMessage}</p>}
 
                 <button type="submit">Submit</button>
+
+                {formStatus && <p className="send-confirm">Message sent!</p>}
             </form>
         </section>
     );
